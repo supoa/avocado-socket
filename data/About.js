3cc4d63cb6a0be6912b5fe07af5ec2a74dfa2a95
sohanr-cyber <sohanur25800@gmail.com>
2022-08-22 21:17:21 +0600

    rename

--- data/About.js
+++ data/About.js
@@ -4,7 +4,7 @@ const about = {
   content1:
     "We are an international financial company engaged in investment activities, which are related to trading on financial markets and cryptocurrency exchanges performed by qualified professional traders",
   content2:
-    "Our goal is to provide our investors with a reliable source of high income, while minimizing any possible risks and offering a high-quality service, allowing us to automate and simplify the relations between the investors and the trustees. We work towards increasing your profit margin by profitable investment. We look forward to you being part of our community.",
+    "Our goal is to provide you our investors with a reliable source of high income, while minimizing any possible risks and offering a high-quality service, allowing us to automate and simplify the relations between the investors and the trustees. We work towards increasing your profit margin by profitable investment. We look forward to you being part of our community.",
   btn: "MORE INFO",
 };
 
